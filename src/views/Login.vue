0c3c55da549bee0224106bf5145e31e0d05e1d2c
Matias Moreno <matiasmorenog@gmail.com>
2021-03-05 08:58:58 -0300

    wip fb login

---
+++ src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <Modal :show="true" title="Welcome to Your Vue.js App">
+    <template v-slot:header>
+      <h3>Login</h3>
+    </template>
+    <template v-slot:body>
+      <input type="text" name="username" placeholder="Username" />
+      <input type="text" name="password" placeholder="Password" />
+      <div>
+        <input type="checkbox" name="remember" />
+        <label for="remember">Remember me</label>
+        <button>Sign in</button>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <button @click="fbLogin">Sign in with facebook</button>
+
+      <div
+        class="fb-login-button"
+        data-width=""
+        data-size="medium"
+        data-button-type="login_with"
+        data-layout="default"
+        data-auto-logout-link="false"
+        data-use-continue-as="true"
+      ></div>
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue';
+
+import { initFbsdk } from '@/config/fb.js';
+
+export default {
+  name: 'Home',
+  components: {
+    Modal,
+  },
+  async mounted() {
+    await initFbsdk();
+  },
+  watch: {
+    'window.FB.getLoginStatus'(a) {
+      console.log(a);
+    },
+  },
+  computed: {
+    /* checkFbStatus() {
+      window.FB.getLoginStatus(function(response) {
+        return response;
+      });
+    }, */
+  },
+  methods: {
+    fbLogin() {
+      console.log(window.FB);
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background: gray;
+}
+input {
+  margin: 10px;
+}
+</style>
